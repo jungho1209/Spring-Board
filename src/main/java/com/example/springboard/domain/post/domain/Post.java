aba88d00d4c06f3ba1e02e01ea583f019c5efca5
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-07-11 21:55:26 +0900

    ⛏ :: User 의 id 칼럼과 매핑

--- src/main/java/com/example/springboard/domain/post/domain/Post.java
+++ src/main/java/com/example/springboard/domain/post/domain/Post.java
@@ -1,5 +1,6 @@
 package com.example.springboard.domain.post.domain;
 
+import com.example.springboard.domain.user.domain.User;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,15 +17,26 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 30 , nullable = false)
+    @Column(length = 30, nullable = false)
     private String title;
 
-    @Column(length = 1000 , nullable = false)
+    @Column(length = 1000, nullable = false)
     private String content;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
     @Builder
-    public Post(String title, String content) {
+    public Post(String title, String content, User user) {
         this.title = title;
         this.content = content;
+        this.user = user;
+    }
+
+    public void postUpdate(String title, String content) {
+        this.title = title;
+        this.content = content;
+
     }
 }
