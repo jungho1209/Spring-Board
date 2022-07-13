7738da8bd153f2e20e577cafedeb1c1c37009131
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-07-13 23:03:23 +0900

    🚀 :: Custom Error 추가

--- src/main/java/com/example/springboard/global/error/ErrorCode.java
+++ src/main/java/com/example/springboard/global/error/ErrorCode.java
@@ -13,6 +13,8 @@ public enum ErrorCode {
     EXPIRED_JWT(401, "Expired Jwt"),
     INVALID_JWT(401, "Invalid Jwt"),
     PASSWORD_MIS_MATCH(401, "Password Mis Match"),
+    NO_PERMISSION_TO_DELETE_COMMENT(401, "No Permission To Delete Comment"),
+    NO_PERMISSION_TO_MODIFY_COMMENT(401, "No Permission To Modify Comment"),
     INVALID_REFRESH_TOKEN(401 , "Invalid Refresh Token"),
 
     USER_NOT_FOUND(404, "User Not Found"),
@@ -20,13 +22,15 @@ public enum ErrorCode {
     AUTH_NOT_FOUND(404, "Auth Not Found"),
     POST_NOT_FOUND(404, "Post Not Found"),
     COMMENT_NOT_FOUND(404, "Comment Not Found"),
+    ID_NOT_FOUND(404, "Id Mis Match"),
+    ACCOUNTID_NOT_FOUND(404, "AccountId Mis Match"),
 
     USER_EXISTS(409, "User Exists"),
+    NOT_EXIST_ACCOUNT(409, "Account Not Exist"),
 
-    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
-    ID_NOT_FOUND(404, "AccountId Mis Match"),
-    NOT_EXIST_ACCOUNT(409, "Account Not Exist");
+
 
     private final int status;
     private final String message;
