5ca75b971ce2bd57bb9c78bae58480cf172361d0
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-07-19 09:25:48 +0900

    🚀 :: NotNull 제약 조건 추가

--- src/main/java/com/example/springboard/domain/comment/controller/dto/request/CreateCommentRequest.java
+++ src/main/java/com/example/springboard/domain/comment/controller/dto/request/CreateCommentRequest.java
@@ -3,12 +3,14 @@ package com.example.springboard.domain.comment.controller.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 public class CreateCommentRequest {
 
+    @NotNull
     @Size(min = 1, max = 500, message = "comment 는 500자를 넘을 수 없습니다.")
     private String comment;
 
--- src/main/java/com/example/springboard/domain/post/controller/dto/request/PostRequest.java
+++ src/main/java/com/example/springboard/domain/post/controller/dto/request/PostRequest.java
@@ -3,15 +3,20 @@ package com.example.springboard.domain.post.controller.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 public class PostRequest {
 
-    @Size(max = 30 , message = "title 은 최대 30자여야 합니다.")
+    @NotNull
+    @Size(max = 30, message = "title 은 최대 30자여야 합니다.")
     private String title;
 
-    @Size(max = 1000 , message = "content 는 최대 1000자여야 합니다.")
+    @NotNull
+    @Size(max = 1000, message = "content 는 최대 1000자여야 합니다.")
     private String content;
+
+
 }
