5ddf84d1fd7001381714f5abcf11e01da939d3a6
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-07-22 11:04:41 +0900

    🚀 :: 인가 기능 구현

--- src/main/java/com/example/springboard/global/facade/CurrentFacade.java
+++ src/main/java/com/example/springboard/global/facade/CurrentFacade.java
@@ -1,23 +1,21 @@
-package com.example.springboard.global.auth;
+package com.example.springboard.global.facade;
 
 import com.example.springboard.domain.user.domain.User;
 import com.example.springboard.domain.user.domain.repository.UserRepository;
 import com.example.springboard.domain.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
-public class AuthenticationFacade {
+@Component
+public class CurrentFacade {
 
     private final UserRepository userRepository;
 
     public User getCurrentUser() {
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        AuthDetails authDetails = (AuthDetails) authentication.getPrincipal();
-
-        return userRepository.findByAccountId(authDetails.getUsername())
+        String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByAccountId(accountId)
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
     }
 }
